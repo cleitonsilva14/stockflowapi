8624614a5a516441271384f08fa92eefc274f1c2
cleitonsilva14 <cleiton18ads@gmail.com>
2025-11-13 20:34:22 -0300

    Ajustes para 'static'

--- src/main/java/br/com/stockflowapi/exception/handler/ApiExceptionHandler.java
+++ src/main/java/br/com/stockflowapi/exception/handler/ApiExceptionHandler.java
@@ -3,11 +3,12 @@ package br.com.stockflowapi.exception.handler;
 import br.com.stockflowapi.exception.custom.EntityNotFoundException;
 import br.com.stockflowapi.exception.dto.ErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import java.time.LocalDateTime;
 
@@ -17,12 +18,12 @@ public class ApiExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException exception, HttpServletRequest request){
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .contentType(MediaType.APPLICATION_JSON)
+                .status(NOT_FOUND)
+                .contentType(APPLICATION_JSON)
                 .body(ErrorMessage.builder()
                         .timestamp(LocalDateTime.now())
-                        .code(HttpStatus.NOT_FOUND.value())
-                        .status(HttpStatus.NOT_FOUND.name())
+                        .code(NOT_FOUND.value())
+                        .status(NOT_FOUND.name())
                         .method(request.getMethod())
                         .requestURI(request.getRequestURI())
                         .message(exception.getMessage())
