8cb4398d57e350f9a29660ce805f2b5b54b4b492
cleitonsilva14 <cleiton18ads@gmail.com>
2025-11-15 12:12:02 -0300

    Ajustes record Error Message

--- src/main/java/br/com/stockflowapi/exception/dto/ErrorMessage.java
+++ src/main/java/br/com/stockflowapi/exception/dto/ErrorMessage.java
@@ -1,15 +1,37 @@
 package br.com.stockflowapi.exception.dto;
 
 import lombok.Builder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@Builder
+@Builder(toBuilder = true)
 public record ErrorMessage(
-    LocalDateTime timestamp,
-    Integer code,
-    String status,
-    String message,
-    String method,
-    String requestURI
-) { }
+        LocalDateTime timestamp,
+        Integer code,
+        String status,
+        String message,
+        String method,
+        String requestURI,
+        Map<String, String> errors
+) {
+
+    public ErrorMessage addErrors(BindingResult bindingResult) {
+        Map<String, String> newErrors = new HashMap<>();
+        if (this.errors != null) {
+            newErrors.putAll(this.errors);
+        }
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            newErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return this.toBuilder()
+                .errors(newErrors)
+                .build();
+    }
+
+}
--- src/main/java/br/com/stockflowapi/exception/handler/ApiExceptionHandler.java
+++ src/main/java/br/com/stockflowapi/exception/handler/ApiExceptionHandler.java
@@ -4,16 +4,38 @@ import br.com.stockflowapi.exception.custom.EntityNotFoundException;
 import br.com.stockflowapi.exception.dto.ErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException exception,
+                                                                HttpServletRequest request,
+                                                                BindingResult bindingResult){
+        return ResponseEntity
+                .status(UNPROCESSABLE_ENTITY)
+                .contentType(APPLICATION_JSON)
+                .body(ErrorMessage.builder()
+                        .timestamp(LocalDateTime.now())
+                        .code(UNPROCESSABLE_ENTITY.value())
+                        .status(UNPROCESSABLE_ENTITY.name())
+                        .method(request.getMethod())
+                        .requestURI(request.getRequestURI())
+                        .errors(new HashMap<>())
+                        .message(exception.getMessage())
+                        .build().addErrors(bindingResult));
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException exception,
@@ -30,6 +52,7 @@ public class ApiExceptionHandler {
                         .message(exception.getMessage())
                         .build());
     }
+
 
 
 }
