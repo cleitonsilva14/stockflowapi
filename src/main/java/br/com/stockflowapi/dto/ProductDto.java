99599bf593f0d1c3ee585b4db0f39f084a50f3dc
cleitonsilva14 <cleiton18ads@gmail.com>
2025-11-17 11:21:23 -0300

    Aplicando @Schema(example=...) no DTO para exemplificar os dados de envio no POST

--- src/main/java/br/com/stockflowapi/dto/ProductDto.java
+++ src/main/java/br/com/stockflowapi/dto/ProductDto.java
@@ -1,5 +1,7 @@
 package br.com.stockflowapi.dto;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,10 +12,10 @@ import java.util.List;
 
 @Builder
 public record ProductDto(
-        @NotNull Long code,
-        @NotBlank String name,
-        @NotBlank String description,
-        @DecimalMin("0.01") BigDecimal price,
-        List<String> images
+        @NotNull @Schema(example = "1000") Long code,
+        @NotBlank @Schema(example = "SSD 1TB") String name,
+        @NotBlank @Schema(example = "Product description") String description,
+        @DecimalMin("0.01") @Schema(example = "499.99") BigDecimal price,
+        @ArraySchema(schema = @Schema(example = "https://storage.com/image1.jpg")) List<String> images
 ) {
 }
