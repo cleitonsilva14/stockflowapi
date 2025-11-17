d5eccd12e1c727e1d10ac30d3806c39c68729e35
cleitonsilva14 <cleiton18ads@gmail.com>
2025-11-17 14:04:03 -0300

    Adicionada anotação para explicar  @Mapping(source=..., target=...)

--- src/main/java/br/com/stockflowapi/mapper/ProductMapper.java
+++ src/main/java/br/com/stockflowapi/mapper/ProductMapper.java
@@ -8,6 +8,13 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
+    // o campo DTO categoryId deve receber o valor de product.category.id
+    // source: É o campo de onde os dados vêm, no objeto de entrada (Product).
+    // Pegue o atributo id dentro do objeto category do Product. Ou seja, equivalente a: product.getCategory().getId()
+    // target: É o campo para onde os dados vão, no objeto de saída (ProductDto):
+    // Ou seja: Coloque esse valor (category.id) no campo categoryId do DTO.
+
+
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
 
